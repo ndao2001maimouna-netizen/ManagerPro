14a0251ff25eb53b32b356b607a356b495994afd
maimouna <ndao2001maimouma@gmail.com>
2026-08-16 14:13:10 +0000

    feat(entity): creation des entites POO avec encapsulation et methodes metier

--- src/core/Database.php
+++ src/core/Database.php
@@ -1,30 +1,31 @@
 <?php
 
-class Database{
+class Database {
     private static ?PDO $pdo = null;
 
-    public static function getConnexion():PDO{
-        if(self::$pdo == null){
+    private function __construct() {}
 
+    private function __clone() {}
+
+    public static function getConnexion(): PDO {
+        if (self::$pdo === null) {
             try {
-
                 self::$pdo = new PDO(
-                        "pgsql:host=localhost;dbname=managerpro;port=5432",
-                        "postgres",
-                        "user123"
-                    );
-                    echo 'connexion postgres';
-                  
+                    "pgsql:host=localhost;dbname=managerpro;port=5432",
+                    "postgres",
+                    "user123"
+                );
             } 
             catch (\Throwable $th) {
-                self::$pdo = new PDO("sqlite:".dirname(dirname(__DIR__))."/erp.db");
-               echo 'connexion sqlite';
+                $cheminSqlite = dirname(dirname(__DIR__)) . "/erp.db";
+                self::$pdo = new PDO("sqlite:" . $cheminSqlite);
             }
-            self::$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
-            self::$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
             
-            }
-     return self::$pdo; 
+            self::$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        
+        return self::$pdo; 
     }
 }
 
